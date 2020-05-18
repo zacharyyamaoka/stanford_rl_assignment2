2b1a19417f20eccdcc71484c3a01f085a9042d48
Zach Yamaoka <zach.yamaoka@gmail.com>
2020-05-18 05:21:57 +0100

    commiting what exists

---
+++ test_tf2_linear.py
@@ -0,0 +1,279 @@
+import unittest
+from tf2_linear import Linear
+import numpy as np
+
+
+class TestLinearMetheods(unittest.TestCase):
+
+    # def test_update(self):
+    #
+    #     model = Linear(verbose=0)
+    #
+    #     # test loss decreases on the same state
+    #     curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+    #     reward = 0
+    #     action = 0
+    #     next_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+    #     done = True
+    #
+    #     # loss = 0.5(reward - Q(s,a))**2
+    #
+    #     for i in range(5):
+    #         loss = model.update(curr_state, action, reward, next_state, done)
+    #         if i != 0:
+    #             # print(i, loss, last_loss)
+    #             self.assertTrue(loss <= last_loss)
+    #         last_loss = loss
+    #
+    #     # loss = 0.5((reward + gamma*max Q(s',a) - Q(s,a)))**2
+    #
+    #     curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+    #     reward = 5
+    #     # action = 2 # Use Greedy action so both are max
+    #     next_state = curr_state
+    #     done = False
+    #
+    #     expected_loss = 0.5 * reward**2
+    #
+    #     for i in range(10):
+    #         # update target network so they are the same
+    #         model.update_target_weights()
+    #         action, action_values = model.greedy_policy(curr_state)
+    #         loss = model.update(curr_state, action, reward, next_state, done)
+    #         self.assertTrue(abs(loss-expected_loss) < 0.00001)
+    #
+    # def test_update_target_weights(self):
+    #     model = Linear(verbose=0)
+    #
+    #     # test loss decreases on the same state
+    #     curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+    #     reward = 0
+    #     next_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+    #     done = True
+    #
+    #     # loss = 0.5(reward - Q(s,a))**2
+    #     curr_state_temp = model.preprocess(curr_state)
+    #
+    #     target_pred = model.target_model(curr_state_temp).numpy()
+    #     reg_pred = model.model(curr_state_temp).numpy()
+    #     # print(target_pred, reg_pred)
+    #     self.assertTrue(np.all(target_pred == reg_pred))
+    #
+    #     for i in range(50):
+    #         action = np.random.randint(5)
+    #         loss = model.update(curr_state, action, reward, next_state, done)
+    #
+    #     target_pred = model.target_model(curr_state_temp).numpy()
+    #     reg_pred = model.model(curr_state_temp).numpy()
+    #
+    #     self.assertFalse(np.any(target_pred == reg_pred))
+    #     model.update_target_weights()
+    #
+    #     target_pred = model.target_model(curr_state_temp).numpy()
+    #     self.assertTrue(np.all(target_pred == reg_pred))
+    #
+
+    def test_greedy_policy(self):
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        model = Linear(espilon=0, verbose=0)
+
+        for i in range(10):
+            action, action_values = model.greedy_policy(curr_state)
+            self.assertEqual(action, np.argmax(action_values))
+
+    def test_preprocess(self):
+
+        model = Linear(espilon=0, verbose=0)
+
+        for i in range(10):
+            curr_state = np.random.randint(
+                0, 255, [25, 5, 5, 1], dtype=np.uint16)
+            curr_state = model.preprocess(curr_state)
+
+            len = curr_state.shape[1]
+            channel_mean = np.average(curr_state, axis=(1, 2))
+            self.assertTrue(abs(np.max(channel_mean)) < 0.00001)
+            mean = np.average(curr_state)
+            max = np.amax(curr_state)
+            self.assertTrue(abs(mean) < 0.00001)
+            self.assertEqual(max, 1.0)
+
+    def test_replay_update_target_weights(self):
+        buffer_len = 50
+        model = Linear(replay_buffer_len=buffer_len, verbose=0)
+
+        # test loss decreases on the same state
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        reward = 0
+        next_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        done = True
+
+        for i in range(buffer_len):  # must use a different action to update all parts of network
+            action = np.random.randint(5)
+            model.replay_fill(curr_state, action, reward, next_state, done)
+
+        curr_state_temp = model.preprocess(curr_state)
+        target_pred = model.target_model(curr_state_temp).numpy()
+        reg_pred = model.model(curr_state_temp).numpy()
+        # print(target_pred, reg_pred)
+        self.assertTrue(np.all(target_pred == reg_pred))
+
+        for i in range(20):
+            loss = model.replay_update()
+
+        target_pred = model.target_model(curr_state_temp).numpy()
+        reg_pred = model.model(curr_state_temp).numpy()
+
+        self.assertFalse(np.any(target_pred == reg_pred))
+        model.update_target_weights()
+
+        target_pred = model.target_model(curr_state_temp).numpy()
+        self.assertTrue(np.all(target_pred == reg_pred))
+
+    def test_policy(self):
+
+        # in Greedy Case
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        model = Linear(espilon=0, verbose=0)
+
+        for i in range(10):
+            action, action_values = model.policy(curr_state)
+            self.assertEqual(action, np.argmax(action_values))
+
+        # In Pure Exploratory Case
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        model = Linear(espilon=1, verbose=0)
+
+        iter = 50
+        num_actions = 5
+        max_count = (iter / num_actions) * 2
+        count = 0
+        for i in range(iter):
+            action, action_values = model.policy(curr_state)
+
+            if action == np.argmax(action_values):
+                count += 1
+
+        self.assertLess(count, max_count, msg="Picking Greedy action to often, \
+        mabye run one more time to account for random sampling")
+
+    def test_replay_fill(self):
+
+        buffer_len = 10
+        model = Linear(replay_buffer_len=buffer_len, verbose=0)
+
+        # test loss decreases on the same state
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        action = 1
+        reward = 0
+        next_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        done = True
+
+        model.replay_fill(curr_state, action, reward, next_state, done)
+        self.assertTrue(model.replay_buffer.num_in_buffer == 1)
+        model.replay_fill(curr_state, action, reward, next_state, done)
+        model.replay_fill(curr_state, action, reward, next_state, done)
+        model.replay_fill(curr_state, action, reward, next_state, done)
+        self.assertTrue(model.replay_buffer.num_in_buffer == 4)
+        for i in range(50):
+            model.replay_fill(curr_state, action, reward, next_state, done)
+        self.assertTrue(model.replay_buffer.num_in_buffer == buffer_len)
+
+    def test_replay_sample(self):
+        buffer_len = 10
+        model = Linear(replay_buffer_len=buffer_len, verbose=0)
+
+        # test loss decreases on the same state
+        curr_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        action = 1
+        reward = 0
+        next_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        done = False
+        next_next_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        model.replay_fill(curr_state, action, reward, next_state, done)
+        model.replay_fill(next_state, action, reward, next_next_state, done)
+
+        obs_batch, act_batch, rew_batch, next_obs_batch, done_mask = \
+            model.replay_buffer.sample(1)
+
+        self.assertTrue(np.all(curr_state == obs_batch[0, :, :, :]))
+        self.assertTrue(action == act_batch[0])
+        self.assertTrue(reward == rew_batch[0])
+        self.assertTrue(done == done_mask[0])
+
+        self.assertTrue(np.all(next_state ==
+                               next_obs_batch[0, :, :, :]))
+
+        # Agian
+
+        buffer_len = 10
+        model = Linear(replay_buffer_len=buffer_len, verbose=0)
+
+        # test loss decreases on the same state
+        curr_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        action = 1
+        reward = 0
+        next_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        done = True
+        next_next_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        model.replay_fill(curr_state, action, reward, curr_state, done)
+        model.replay_fill(curr_state, action, reward, next_state, done)
+
+        obs_batch, act_batch, rew_batch, next_obs_batch, done_mask = \
+            model.replay_buffer.sample(1)
+
+        self.assertTrue(np.all(curr_state == obs_batch[0, :, :, :]))
+        self.assertTrue(action == act_batch[0])
+        self.assertTrue(reward == rew_batch[0])
+        self.assertTrue(done == done_mask[0])
+        self.assertTrue(np.all(curr_state ==
+                               next_obs_batch[0, :, :, :]))
+
+    def test_replay_update(self):
+        buffer_len = 50
+        model = Linear(replay_buffer_len=buffer_len,
+                       learning_rate=0.001, verbose=0)
+
+        # test loss decreases on the same state
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        action = 1
+        reward = 0
+        next_state = np.random.randint(0, 50, [5, 5, 1], dtype=np.uint16)
+        done = True
+
+        # prepopulate buffer
+        for i in range(buffer_len):
+            model.replay_fill(curr_state, action, reward, next_state, done)
+
+        # loss = 0.5(reward - Q(s,a))**2
+
+        for i in range(5):
+            loss = model.replay_update()
+            if i != 0:
+                self.assertTrue(loss <= last_loss)
+            last_loss = loss
+
+        # loss = 0.5((reward + gamma*max Q(s',a) - Q(s,a)))**2
+
+        curr_state = np.random.randint(0, 255, [5, 5, 1], dtype=np.uint16)
+        reward = 5
+        # action = 2 # Use Greedy action so both are max
+        next_state = curr_state
+        done = False
+
+        # prepopulate buffer
+        for i in range(buffer_len):
+            model.replay_fill(curr_state, action, reward, next_state, done)
+
+        expected_loss = 0.5 * reward**2
+
+        for i in range(10):
+            # update target network so they are the same
+            model.update_target_weights()
+            action, action_values = model.greedy_policy(curr_state)
+            loss = model.update(curr_state, action, reward, next_state, done)
+            self.assertTrue(abs(loss-expected_loss) < 0.00001)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tf2_linear.py
@@ -0,0 +1,187 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import datetime
+from collections import deque
+from utils.replay_buffer import ReplayBuffer
+
+
+class Linear():
+
+    def __init__(self, replay_buffer_len=5000, learning_rate=0.001, espilon=1, verbose=0):
+        self.gamma = 1
+        self.epsilon = espilon
+        self.min_epsilon = 0.1
+        self.delta_epsilon = 0.025
+        self.epsilon_update_freq = 5000
+
+        self.lr = learning_rate
+        self.target_update_freq = 250
+        self.Optimizer = keras.optimizers.RMSprop(
+            learning_rate=self.lr)
+
+        self.model = keras.Sequential(
+            [
+                keras.layers.Flatten(input_shape=[5, 5, 1]),
+                keras.layers.Dense(
+                    units=5, name="layer1"),
+            ]
+        )
+
+        self.replay_buffer = ReplayBuffer(
+            size=replay_buffer_len, frame_history_len=1)
+        self.mini_batch_size = 32
+
+        if verbose > 0:
+            self.model.summary()
+
+        self.target_model = keras.models.clone_model(self.model)
+        self.update_target_weights()
+
+        log_dir = "logs/experiment/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.Writer = tf.summary.create_file_writer(log_dir)
+        self.update_steps = 0
+        self.update_freq = 2
+        self.total_reward = 0
+
+    def greedy_policy(self, curr_state):  # Espilon Greedy
+        random_val = np.random.uniform()
+
+        if random_val < 0:
+            action = np.random.randint(5)
+            action_values = np.zeros(5)
+
+        else:
+            curr_state = self.preprocess(curr_state)
+            action_values = self.model(curr_state).numpy()
+            action = np.argmax(action_values)
+
+        return action, action_values
+
+    def policy(self, curr_state):  # Espilon Greedy
+
+        random_val = np.random.uniform()
+
+        if random_val < self.epsilon:
+            action = np.random.randint(5)
+            action_values = np.zeros(5)
+
+        else:
+            curr_state = self.preprocess(curr_state)
+            action_values = self.model(curr_state).numpy()
+            action = np.argmax(action_values)
+
+        return action, action_values
+
+    def update_epsilon(self):
+        self.epsilon -= self.delta_epsilon
+        self.epsilon = max(self.epsilon, self.min_epsilon)
+
+    def update_target_weights(self):
+        self.target_model.set_weights(self.model.get_weights())
+
+    def schedule(self, step):
+
+        if step % self.epsilon_update_freq == 0:
+            self.update_epsilon()
+            print("UPDATING EPSILON: ", self.epsilon, "step: ", step)
+
+        if step % self.target_update_freq == 0:
+            self.update_target_weights()
+            print("UPDATING TARGET WEIGHTS step: ", step)
+
+    def preprocess(self, state):
+        """
+        Change image values for better numerical properties:
+        1. Mean center
+        2. Normalize between for max 1
+        """
+
+        if len(state.shape) == 3:
+            state = np.expand_dims(state, axis=0)
+
+        B = state.shape[0]
+        W = state.shape[1]
+        H = state.shape[2]
+        D = state.shape[3]
+
+        state = state - np.mean(state, axis=(1, 2)
+                                ).reshape(B, 1, 1, D)
+
+        state = state / np.amax(state, axis=(1, 2)
+                                ).reshape(B, 1, 1, D)
+
+        state = state.astype("float32")
+        return state
+
+    def replay_fill(self, curr_state, action, reward, next_state, done):
+        # store next_state, on the next_update to avoid dupilcation
+        # curr_state_processed = self.preprocess(curr_state)
+        idx = self.replay_buffer.store_frame(curr_state)
+        self.replay_buffer.store_effect(idx, action, reward, done)
+
+    def replay_update(self):
+
+        self.update_steps += 1
+
+        # Sample mini-batch of experince from buffer
+        obs_batch, act_batch, rew_batch, next_obs_batch, done_mask = \
+            self.replay_buffer.sample(self.mini_batch_size)
+
+        obs_batch = self.preprocess(obs_batch)
+        next_obs_batch = self.preprocess(next_obs_batch)
+
+        with tf.GradientTape() as tape:
+
+            action_values = self.model(obs_batch)  # (batch_size, 5)
+            pred_value = tf.gather(action_values, act_batch, axis=1)
+
+            target_value = rew_batch
+            future_value = self.gamma * \
+                np.amax(self.target_model(next_obs_batch).numpy(), axis=1)
+            target_value[np.logical_not(
+                done_mask)] += future_value[np.logical_not(done_mask)]
+
+            loss = tf.math.reduce_mean(0.5 * (target_value - pred_value)**2)
+
+        gradients = tape.gradient(loss, self.model.trainable_weights)
+
+        self.Optimizer.apply_gradients(
+            zip(gradients, self.model.trainable_weights))
+
+        self.schedule(self.update_steps)
+
+        return loss.numpy()
+
+    def update(self, curr_state, action, reward, next_state, done):
+
+        self.update_steps += 1
+
+        with tf.GradientTape() as tape:
+            curr_state = self.preprocess(curr_state)
+            next_state = self.preprocess(next_state)
+
+            if len(curr_state.shape) == 3:
+                curr_state = np.expand_dims(curr_state, axis=0)
+                next_state = np.expand_dims(next_state, axis=0)
+
+            curr_state = np.expand_dims(curr_state, axis=0)
+            action_values = self.model(curr_state)
+            pred_value = action_values[:, action]
+
+            target_value = reward
+            if not done:
+                target_value += self.gamma * \
+                    tf.math.reduce_max(self.target_model(next_state)).numpy()
+
+            loss = tf.math.reduce_sum(0.5 * (target_value - pred_value)**2)
+
+        gradients = tape.gradient(loss, self.model.trainable_weights)
+        # processed_graxwds = [tf.clip_by_norm(g, 1) for g in gradients]
+
+        self.Optimizer.apply_gradients(
+            zip(gradients, self.model.trainable_weights))
+
+        self.schedule(self.update_steps)
+
+        return loss.numpy()
